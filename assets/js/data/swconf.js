2b917064091f147505ec7fdd43d140f2b7652161
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-02 22:16:16 +0900

    Design: favicon 추가

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738499529',resources: [
+    cacheName: 'chirpy-1738502134',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
