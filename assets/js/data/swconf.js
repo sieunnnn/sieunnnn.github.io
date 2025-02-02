fa47648d00bd2ceac9abd265937adc4a05460518
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-02 22:43:21 +0900

    Fix: favicon 경로 이슈 해결

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738503473',resources: [
+    cacheName: 'chirpy-1738503779',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
