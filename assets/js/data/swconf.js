180efa49c483cdb781d83a085fe4c475bc6a940b
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-02 21:33:31 +0900

    fix: 이미지 경로 문제 수정

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738493469',resources: [
+    cacheName: 'chirpy-1738499529',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
