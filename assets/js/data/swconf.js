7cf209fdfb96a3080a84667d7690e52ddad3b36b
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-02 23:59:47 +0900

    Feat: 카운터 추가

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738505698',resources: [
+    cacheName: 'chirpy-1738508314',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
