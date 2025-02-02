e9133b55e7023fa0c28283782c83350cd61c0751
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-03 00:44:27 +0900

    Test: 글 생성 테스트

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738510589',resources: [
+    cacheName: 'chirpy-1738511028',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
