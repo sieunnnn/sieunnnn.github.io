d8333ef4401a3798cef70686a88fc7c1a38465e8
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-05 15:10:56 +0900

    🚀 게시글 자동배포: 2025-02-05 15:10:56

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738645905',resources: [
+    cacheName: 'chirpy-1738735838',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
