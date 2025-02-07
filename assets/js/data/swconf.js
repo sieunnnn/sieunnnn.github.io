ba90821a07d19dcf117693617e077b8b6ffcbe71
sieunnnn <wldsmtldsm65@gmail.com>
2025-02-07 11:02:06 +0900

    🚀 게시글 자동배포: 2025-02-07 11:02:06

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738892903',resources: [
+    cacheName: 'chirpy-1738893651',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- move-file.sh
+++ move-file.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "🔄 URL 수정 중..."
+export LC_CTYPE=C
+export LANG=C
 find _site -type f -exec sed -i '' 's|https://sieunnnn.github.io|https://sieunnnn.github.io|g' {} +
+
 
 echo "📂 배포 폴더로 이동 중..."
 cp -r _site/* ../../Desktop/sieunnnn.github.io
